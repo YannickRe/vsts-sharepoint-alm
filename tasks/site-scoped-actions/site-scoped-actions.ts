ac390c50e53ca7989c5c942aff7cfe2a1a0136f1
Yannick Reekmans <reekmans.yannick@gmail.com>
2018-01-21 00:28:31 +0100

    Read parameters Implemented logging for the parameters

--- tasks/site-scoped-actions/site-scoped-actions.ts
+++ tasks/site-scoped-actions/site-scoped-actions.ts
@@ -5,7 +5,17 @@ import * as tl from 'vsts-task-lib/task';
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 async function main(): Promise<void> {
-    console.log("Executing action in context of the SharePoint Site");
+	var action = tl.getInput('action', true);
+	var spSiteConnection: string = tl.getInput('spSiteConnection', true);
+	var spSiteUrl: string = tl.getEndpointUrl(spSiteConnection, false);
+	var spSiteUsername: string = tl.getEndpointAuthorizationParameter(spSiteConnection, "username", false);
+	var spSitePassword: string = tl.getEndpointAuthorizationParameter(spSiteConnection, "password", false);
+	var packageIdIn: string = tl.getInput("packageIdIn", true);
+
+	console.log(`Action: ${action}`);
+	console.log(`App Catalog Connection: ${spSiteConnection}`);
+	console.log(`App Catalog Url: ${spSiteUrl}`);
+	console.log(`Package Id In: ${packageIdIn}`);
 }
 
 main()
--- tasks/tenant-scoped-actions/tenant-scoped-actions.ts
+++ tasks/tenant-scoped-actions/tenant-scoped-actions.ts
@@ -1,11 +1,48 @@
 var path = require('path')
-
 import * as tl from 'vsts-task-lib/task';
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 async function main(): Promise<void> {
-    console.log("Executing action in context of the SharePoint Tenant App Catalog");
+	var action = tl.getInput('action', true);
+	var appCatalogConnection: string = tl.getInput('appCatalogConnection', true);
+	var appCatalogUrl: string = tl.getEndpointUrl(appCatalogConnection, false);
+	var appCatalogUsername: string = tl.getEndpointAuthorizationParameter(appCatalogConnection, "username", false);
+	var appCatalogPassword: string = tl.getEndpointAuthorizationParameter(appCatalogConnection, "password", false);
+	var appFilePath: string = (action === "Add") ? tl.getPathInput('appFilePath', true, false).trim() : null;
+	var overwriteExisting: boolean = (action === "Add") ? tl.getBoolInput("overwriteExisting", true) : null;
+	var packageIdOut: string = (action === "Add") ? tl.getInput("packageIdOut", true) : null;
+	var skipFeatureDeployment: boolean = (action === "Deploy") ? tl.getBoolInput("skipFeatureDeployment", true) : null;
+	var packageIdIn: string = (action !== "Add") ? tl.getInput("packageIdIn", true) : null;
+
+	console.log(`Action: ${action}`);
+	console.log(`App Catalog Connection: ${appCatalogConnection}`);
+	console.log(`App Catalog Url: ${appCatalogUrl}`);
+
+	switch(action)
+	{
+		case "Add": {
+			console.log(`App File Path: ${appFilePath}`);
+			console.log(`Overwrite Existing Package: ${overwriteExisting}`);
+			console.log(`Package Id Out: ${packageIdOut}`);
+			break;
+		}
+		case "Deploy": {
+			console.log(`Package Id In: ${packageIdIn}`);
+			console.log(`Skip Feature Deployment: ${skipFeatureDeployment}`);
+			break;
+		}
+		case "Retract": {
+			console.log(`Package Id In: ${packageIdIn}`);
+			break;
+		}
+		case "Remove": {
+			console.log(`Package Id In: ${packageIdIn}`);
+			break;
+		}
+	}
+
+	console.log("Action completed");
 }
 
 main()
